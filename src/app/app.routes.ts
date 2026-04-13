8581050ae63a4a43b8817d137691897fdfebec65
okemwabrian <okemwabrian135@gmail.com>
2026-04-14 01:13:52 +0300

    Publish portfolio to GitHub

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  
+  // These map the paths to your generated components
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'contact', component: Contact },
+  
+  // A catch-all route just in case someone types a wrong URL
+  { path: '**', redirectTo: '/home' }
+];
